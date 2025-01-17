e0eb8f1fcbfc3d2a61184bcd6d958718db3ad615
srinathreddygona <150712018+srinathreddygona@users.noreply.github.com>
2025-01-17 15:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import postRoutes from './routes/posts.js'; 
+
 import dotenv from 'dotenv';
 
 
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({limit:"30mb",extended:true}));
 
 app.use(cors());
 app.use('/posts',postRoutes);
+app.use('/user',userRoutes);
 
 
 
