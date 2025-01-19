654df0494922beb48e753cfe1d3815feb5b055e1
srinathreddygona <gsrinathreddy12@gmail.com>
2025-01-19 16:00:08 +0530

    didpagination

--- routes/posts.js
+++ routes/posts.js
@@ -1,10 +1,11 @@
  import express from "express";
  import auth from "../middleware/auth.js";
- import { getPosts ,createPost,updatePost,deletePost,likePost} from "../controllers/posts.js";
+ import { getPostsBySearch,getPosts ,getPost,createPost,updatePost,deletePost,likePost} from "../controllers/posts.js";
  const router=express.Router();
  
-
+ router.get('/search',getPostsBySearch);
  router.get('/',getPosts);
+ router.get('/:id',getPost);
  router.post('/',auth,createPost);
  router.patch('/:id',auth,updatePost);
  router.delete('/:id',auth,deletePost);
